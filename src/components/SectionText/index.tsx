f42f82baa59be83ffc4ef2ebe504f4c9cf8d45e3
PauloHOAlmeida <paulo.almeida@epiousion.com.br>
2024-10-10 22:55:54 -0300

    :sparkles: feat: update career

--- src/components/SectionText/index.tsx
+++ src/components/SectionText/index.tsx
@@ -9,6 +9,8 @@ interface SectionProp {
 
 export const SectionText = ({ image, title, id, children }: SectionProp) => (
 	<Section image={image} title={title} id={id}>
-		<p className="text-base md:text-2xl">{children}</p>
+		<div className='text-base md:text-2xl space-y-4'>
+			{children}
+		</div>
 	</Section>
 );
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -32,7 +32,7 @@ function Home() {
 
 			<main className="flex flex-col p-4 sm:p-8 md:p-16 bg-white relative mb-16 divider-bottom" id="inicio">
 				<figure className="flex flex-wrap items-center relative">
-						<Image src="/profile.webp" alt="Foto de Paulo Henrique" />
+					<Image src="/profile.webp" alt="Foto de Paulo Henrique" />
 					<div className="absolute top-1/2 left-1/2">
 						<h3 className="bg-white text-black md:text-2xl">Olá, sou</h3>
 						<h1 className="bg-primary text-white text-4xl md:text-6xl">
@@ -51,43 +51,87 @@ function Home() {
 					<p className="text-black md:text-2xl">
 						E estou muito feliz se saber de sua visita <FontAwesomeIcon icon={faSmile} />
 					</p>
-					<Button href="/CV_PAULO_ALMEIDA.pdf" icon={faDownload} text="Baixar currículo" variation="black" download/>
+					<Button href="/CV_PAULO_ALMEIDA.pdf" icon={faDownload} text="Baixar currículo" variation="black" download />
 				</div>
 			</main>
 
 			<SectionText image="/euSou.webp" title="Eu sou...">
-				Sou Desenvolvedor Fullstack Júnior com foco em Typescript, estudante de Análise e Desenvolvimento de Sistemas e técnico em informática
-				para internet.
-				Tenho 18 anos e resido atualmente em Sorocaba - SP, embora seja ainda bem jovem já possuo uma experiência considerável em desenvolvimento que conquistei por meio de 
-				freelances, trabalhos acadêmicos/voluntários e estágio,
-				tendo atuado em diversos projetos interessantes, como o PAM (software utilizado pelo Corpo de Bombeiros), Monstronautas (plataforma de educação gameficada) e 
-				Ümore (protótipo de plataforma de controle emocional), no momento sou estagiário no Instituto Edutech21, startup que desenvolve games em realidade virtual para o ensino matemático.
+				<p>
+					Sou Desenvolvedor Fullstack, proeficiente em Javascript/Typescript e PHP, estudante de Análise e Desenvolvimento de Sistemas, técnico em informática
+					para internet, criador do Raven Framework e um futuro arquiteto de software.
+				</p>
+
+				<p>
+					Tenho 19 anos e resido atualmente em Sorocaba - SP, embora seja ainda bem jovem possuo grande experiência com desenvolvimento de software,
+					principalmente backend, tenho um amor muito grande por arquitetura, tendo também já desenhado alguns sistemas.
+				</p>
+
+				<p>
+					Sempre fui muito estudioso e comecei na programação aos 14 anos, fazendo alguns jogos por hobbie enquanto também vendia alguns sistemas mais básicos para pequenos comércios de minha cidade natal,
+					um projeto feito nessa época que me orgulho muito até hoje é o "Monstronautas", uma plataforma de ensino infantil.
+				</p>
+
+				<p>
+					Em 2023 comecei a universidade, vindo embora para Sorocaba - SP para melhores oportunidades,
+					aqui fiz alguns trabalhos voluntários para minha universidade, entre eles se destaca o "PAM", um software de gerenciamento para o Plano de Auxílio Mútuo da região, nessa época também comecei como estagiário numa startup de desenvolvimento de jogos.
+				</p>
+
+				<p>
+					Atualmente sou desenvolvedor Fullstack na Epiousion IT, trabalhando principalmente com Nest, React, PHP e Linux, além disso, sou o criador e, por enquanto, o único mantenedor do "Raven Framework", um framework PHP altamente baseado em Nest,
+					que tem por filosofia ser fácil, simples, completo, "montável" e completamente open-source.
+				</p>
 			</SectionText>
 
 			<SectionText image="/objetivo.webp" title="O meu objetivo:" id="objetivos">
-				Meu principal objetivo atualmente é crescer na carreira para que possa aprender mais e entregar produtos cada vez melhores, por isso estou em busca de uma vaga como 
-				desenvolvedor júnior, entretanto, meu sonho profissional é criar meu próprio produto, algo que realmente possa facilitar a vida das pessoas, algo que tenha mais que apenas valor de mercado.
+				<p>
+					Meu objetivo no momento é me aperfeiçoar cada vez mais e me especializar em arquitetura de software, no momento minha grande meta é ser sênior até os 25 anos, entretanto,
+					acredito profundamente que ainda criarei uma empresa de muito valor que poderá realmente trazer algo positivo para a sociedade, meu grande objetivo é deixar a minha marca nas pessoas que puder ajudar.
+				</p>
 			</SectionText>
 
 			<TimelineContainer title="Minha experiência" id="experiencia">
 				<Timeline
-					office="Desenvolvedor estagiário"
-					company="Instituto Edutech21"
-					startDate="09/2023"
+					office="Desenvolvedor Fullstack"
+					company="Epiousion IT"
+					startDate="04/2024"
+					endDate="current"
+					location="Sorocaba - SP (Híbrido)"
+				>
+					Desenvolvo aplicações escaláveis e de baixo nível de acoplamento, utilizando principalmente: Nest e PHP para backend e React para frontend.<br />
+					Melhoro a qualidade de código e trabalho na manutenção de sistemas legados, tornando os códigos mais simples e aplicando Design Patterns quando necessário.<br />
+					Auxilio no desenvolvimento da arquitetura de novos projetos, pensando no nível de complexidade, desempenho, custo e escalonamento.<br />
+					Auxilio novos desenvolvedores, estando a disposição para ajudá-los na realização de suas tarefas.<br />
+				</Timeline>
+				<Timeline
+					office="Mantenedor do Raven Framework"
+					company='Comunidade'
+					startDate="06/2024"
 					endDate="current"
 					location="Sorocaba - SP"
 				>
-					Auxilio no desenvolvimento dos produtos da empresa, tais como sites e jogos com teor educativo, utilizando
-					principalmente Typescript, React, Node, MYSQL e GDScript.
+					Desenvolvo como hobbie o Raven Framework, um framework em PHP que utiliza em seu interior uma arquitetura baseada na arquitetura de Micro-Kernel, seu foco é o desenvolvimento de API's Rest,
+					fortemente inspirado em Nest.<br />
+					No momento ele conta com um sistema de roteamento com suporte a parâmetros, conversão da requisição para objetos de transferência de dados, validação dos dados da requisição, sistema de segurança e
+					integração com Swagger (em desenvolvimento).
+				</Timeline>
+				<Timeline
+					office="Desenvolvedor estagiário"
+					company="Instituto Edutech21"
+					startDate="09/2023"
+					endDate="04/2024"
+					location="Sorocaba - SP"
+				>
+					Auxiliava no desenvolvimento dos produtos da empresa, tais como sites e jogos com teor educativo, utilizando
+					principalmente Express, Next, MYSQL e GDScript.
 				</Timeline>
 				<Timeline
 					office="Desenvolvedor voluntário"
 					company="GENTI - Grupo de Estudos do Núcleo de TI UNISO"
 					startDate="03/2023"
-					endDate="current"
+					endDate="09/2024"
 					location="Sorocaba - SP"
 				>
-					Auxilio no desenvolvimento de softwares destinados a outros cursos e à comunidade, tendo como exemplo o site
+					Auxiliava no desenvolvimento de softwares destinados a outros cursos e à comunidade, tendo como exemplo o site
 					do PAM, desenvolvido em HTML, CSS, Javascript, PHP e MYSQL.
 				</Timeline>
 				<Timeline
@@ -97,9 +141,9 @@ function Home() {
 					endDate="12/2023"
 					location="Sorocaba - SP"
 				>
-					Auxiliei no desenvolvimento de um protótipo que tem por objetivo o uso de feedbacks emocionais, para a
-					melhoria do ensino universitário.
-					<br /> Auxiliei no desenvolvimento de um software mini-ERP para controle e gestão de ativos utilizando React,
+					Auxiliava no desenvolvimento de um protótipo que tem por objetivo o uso de feedbacks emocionais, para a
+					melhoria do ensino universitário.<br />
+					Auxiliava no desenvolvimento de um software mini-ERP para controle e gestão de ativos utilizando React,
 					Flask e MYSQL
 				</Timeline>
 				<Timeline
@@ -108,7 +152,7 @@ function Home() {
 					endDate="02/2022"
 					location="Ribeirão Branco - SP"
 				>
-					Desenvolvi soluções web para diversas empresas de demasiados setores, utilizando, principalmente HTML, CSS,
+					Desenvolvia soluções web para diversas empresas de demasiados setores, utilizando, principalmente HTML, CSS,
 					JavaScript, PHP, Wordpress e MYSQL.
 				</Timeline>
 			</TimelineContainer>
@@ -121,8 +165,7 @@ function Home() {
 					endDate="06/2025"
 					location="Sorocaba - SP"
 				>
-					Estou aprendendo muita coisa interessante sobre o universo de TI e sobre mercado de trabalho, como SCRUM,
-					KANBAN, segurança da informação, direito digital, infraestrutura de TI, etc.
+					Estou aprendendo muita coisa interessante sobre o universo da TI, como metodologias ágeis, segurança da informação, cultura de DevOps, infraestrutura de TI, estrutura de dados, etc.
 				</Timeline>
 				<Timeline
 					office="Técnico em informática para internet"
