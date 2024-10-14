740ac9f0999f2ce41cd41858bc65d3215b4cca8b
PauloHenriqueOliveiradeAlmeida <paulo333henrique00@gmail.com>
2024-10-13 22:26:54 -0300

    :bug: fix: intern before voluntary on experience section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -57,35 +57,44 @@ function Home() {
 
 			<SectionText image="/euSou.webp" title="Eu sou...">
 				<p>
-					Sou Desenvolvedor Fullstack, proeficiente em Javascript/Typescript e PHP, estudante de Análise e Desenvolvimento de Sistemas, técnico em informática
-					para internet, criador do Raven Framework e um futuro arquiteto de software.
+					Sou Desenvolvedor Fullstack, proeficiente em Javascript/Typescript e PHP, estudante de Análise e
+					Desenvolvimento de Sistemas, técnico em informática para internet, criador do Raven Framework e um futuro
+					arquiteto de software.
 				</p>
 
 				<p>
-					Tenho 19 anos e resido atualmente em Sorocaba - SP, embora seja ainda bem jovem possuo grande experiência com desenvolvimento de software,
-					principalmente backend, tenho um amor muito grande por arquitetura, tendo também já desenhado alguns sistemas.
+					Tenho 19 anos e resido atualmente em Sorocaba - SP, embora seja ainda bem jovem possuo grande experiência com
+					desenvolvimento de software, principalmente backend, tenho um amor muito grande por arquitetura, tendo também
+					já desenhado alguns sistemas.
 				</p>
 
 				<p>
-					Sempre fui muito estudioso e comecei na programação aos 14 anos, fazendo alguns jogos por hobbie enquanto também vendia alguns sistemas mais básicos para pequenos comércios de minha cidade natal,
-					um projeto feito nessa época que me orgulho muito até hoje é o "Monstronautas", uma plataforma de ensino infantil.
+					Sempre fui muito estudioso e comecei na programação aos 14 anos, fazendo alguns jogos por hobbie enquanto
+					também vendia alguns sistemas mais básicos para pequenos comércios de minha cidade natal, um projeto feito
+					nessa época que me orgulho muito até hoje é o "Monstronautas", uma plataforma de ensino infantil.
 				</p>
 
 				<p>
-					Em 2023 comecei a universidade, vindo embora para Sorocaba - SP para melhores oportunidades,
-					aqui fiz alguns trabalhos voluntários para minha universidade, entre eles se destaca o "PAM", um software de gerenciamento para o Plano de Auxílio Mútuo da região, nessa época também comecei como estagiário numa startup de desenvolvimento de jogos.
+					Em 2023 comecei a universidade, vindo embora para Sorocaba - SP para melhores oportunidades, aqui fiz alguns
+					trabalhos voluntários para minha universidade, entre eles se destaca o "PAM", um software de gerenciamento
+					para o Plano de Auxílio Mútuo da região, nessa época também comecei como estagiário numa startup de
+					desenvolvimento de jogos.
 				</p>
 
 				<p>
-					Atualmente sou desenvolvedor Fullstack na Epiousion IT, trabalhando principalmente com Nest, React, PHP e Linux, além disso, sou o criador e, por enquanto, o único mantenedor do "Raven Framework", um framework PHP altamente baseado em Nest,
-					que tem por filosofia ser fácil, simples, completo, "montável" e completamente open-source.
+					Atualmente sou desenvolvedor Fullstack na Epiousion IT, trabalhando principalmente com Nest, React, PHP e
+					Linux, além disso, sou o criador e, por enquanto, o único mantenedor do "Raven Framework", um framework PHP
+					altamente baseado em Nest, que tem por filosofia ser fácil, simples, completo, "montável" e completamente
+					open-source.
 				</p>
 			</SectionText>
 
 			<SectionText image="/objetivo.webp" title="O meu objetivo:" id="objetivos">
 				<p>
-					Meu objetivo no momento é me aperfeiçoar cada vez mais e me especializar em arquitetura de software, no momento minha grande meta é ser sênior até os 25 anos, entretanto,
-					acredito profundamente que ainda criarei uma empresa de muito valor que poderá realmente trazer algo positivo para a sociedade, meu grande objetivo é deixar a minha marca nas pessoas que puder ajudar.
+					Meu objetivo no momento é me aperfeiçoar cada vez mais e me especializar em arquitetura de software, no
+					momento minha grande meta é ser sênior até os 25 anos, entretanto, acredito profundamente que ainda criarei
+					uma empresa de muito valor que poderá realmente trazer algo positivo para a sociedade, meu grande objetivo é
+					deixar a minha marca nas pessoas que puder ajudar.
 				</p>
 			</SectionText>
 
@@ -97,32 +106,32 @@ function Home() {
 					endDate="current"
 					location="Sorocaba - SP (Híbrido)"
 				>
-					Desenvolvo aplicações escaláveis e de baixo nível de acoplamento, utilizando principalmente: Nest e PHP para backend e React para frontend.<br />
-					Melhoro a qualidade de código e trabalho na manutenção de sistemas legados, tornando os códigos mais simples e aplicando Design Patterns quando necessário.<br />
-					Auxilio no desenvolvimento da arquitetura de novos projetos, pensando no nível de complexidade, desempenho, custo e escalonamento.<br />
-					Auxilio novos desenvolvedores, estando a disposição para ajudá-los na realização de suas tarefas.<br />
+					Desenvolvo aplicações escaláveis e de baixo nível de acoplamento, utilizando principalmente: Nest e PHP para
+					backend e React para frontend.
+					<br />
+					Melhoro a qualidade de código e trabalho na manutenção de sistemas legados, tornando os códigos mais simples e
+					aplicando Design Patterns quando necessário.
+					<br />
+					Auxilio no desenvolvimento da arquitetura de novos projetos, pensando no nível de complexidade, desempenho,
+					custo e escalonamento.
+					<br />
+					Auxilio novos desenvolvedores, estando a disposição para ajudá-los na realização de suas tarefas.
+					<br />
 				</Timeline>
 				<Timeline
 					office="Mantenedor do Raven Framework"
-					company='Comunidade'
+					company="Comunidade"
 					startDate="06/2024"
 					endDate="current"
 					location="Sorocaba - SP"
 				>
-					Desenvolvo como hobbie o Raven Framework, um framework em PHP que utiliza em seu interior uma arquitetura baseada na arquitetura de Micro-Kernel, seu foco é o desenvolvimento de API's Rest,
-					fortemente inspirado em Nest.<br />
-					No momento ele conta com um sistema de roteamento com suporte a parâmetros, conversão da requisição para objetos de transferência de dados, validação dos dados da requisição, sistema de segurança e
-					integração com Swagger (em desenvolvimento).
-				</Timeline>
-				<Timeline
-					office="Desenvolvedor estagiário"
-					company="Instituto Edutech21"
-					startDate="09/2023"
-					endDate="04/2024"
-					location="Sorocaba - SP"
-				>
-					Auxiliava no desenvolvimento dos produtos da empresa, tais como sites e jogos com teor educativo, utilizando
-					principalmente Express, Next, MYSQL e GDScript.
+					Desenvolvo como hobbie o Raven Framework, um framework em PHP que utiliza em seu interior uma arquitetura
+					baseada na arquitetura de Micro-Kernel, seu foco é o desenvolvimento de API's Rest, fortemente inspirado em
+					Nest.
+					<br />
+					No momento ele conta com um sistema de roteamento com suporte a parâmetros, conversão da requisição para
+					objetos de transferência de dados, validação dos dados da requisição, sistema de segurança e integração com
+					Swagger (em desenvolvimento).
 				</Timeline>
 				<Timeline
 					office="Desenvolvedor voluntário"
@@ -135,6 +144,16 @@ function Home() {
 					do PAM, desenvolvido em HTML, CSS, Javascript, PHP e MYSQL.
 				</Timeline>
 				<Timeline
+					office="Desenvolvedor estagiário"
+					company="Instituto Edutech21"
+					startDate="09/2023"
+					endDate="04/2024"
+					location="Sorocaba - SP"
+				>
+					Auxiliava no desenvolvimento dos produtos da empresa, tais como sites e jogos com teor educativo, utilizando
+					principalmente Express, Next, MYSQL e GDScript.
+				</Timeline>
+				<Timeline
 					office="Desenvolvedor acadêmico"
 					company="Ümore/Moreü - projeto acadêmico"
 					startDate="03/2023"
@@ -142,9 +161,10 @@ function Home() {
 					location="Sorocaba - SP"
 				>
 					Auxiliava no desenvolvimento de um protótipo que tem por objetivo o uso de feedbacks emocionais, para a
-					melhoria do ensino universitário.<br />
-					Auxiliava no desenvolvimento de um software mini-ERP para controle e gestão de ativos utilizando React,
-					Flask e MYSQL
+					melhoria do ensino universitário.
+					<br />
+					Auxiliava no desenvolvimento de um software mini-ERP para controle e gestão de ativos utilizando React, Flask
+					e MYSQL
 				</Timeline>
 				<Timeline
 					office="Desenvolvedor fullstack freelancer"
@@ -165,7 +185,8 @@ function Home() {
 					endDate="06/2025"
 					location="Sorocaba - SP"
 				>
-					Estou aprendendo muita coisa interessante sobre o universo da TI, como metodologias ágeis, segurança da informação, cultura de DevOps, infraestrutura de TI, estrutura de dados, etc.
+					Estou aprendendo muita coisa interessante sobre o universo da TI, como metodologias ágeis, segurança da
+					informação, cultura de DevOps, infraestrutura de TI, estrutura de dados, etc.
 				</Timeline>
 				<Timeline
 					office="Técnico em informática para internet"
